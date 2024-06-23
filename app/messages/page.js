520e794051c9ecdabaaf91b01f8fdd1782971d8f
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-23 09:18:18 +0330

    revalidateTag was intruduced.

--- app/messages/page.js
+++ app/messages/page.js
@@ -4,12 +4,11 @@ export default async function MessagesPage() {
   // there is a same request in layout.js file. but next just sends one of them and uses the response in all of application environment.
 
   const response = await fetch("http://localhost:8080/messages", {
-
     // cache: "force-cache" or "no-store"
     // ------------------------------------------
     // next: {
-    //   revalidate: truty | number
-    //    if number: Set the cache lifetime of a resource (in seconds).
+    //   revalidate: truty | number => if number: Set the cache lifetime of a resource (in seconds).
+    // tags: [array of strings] => revalidate a specific request with specific tags instead of revalidatePath.
     // }
   });
 
