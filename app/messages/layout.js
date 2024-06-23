a601f457d3319ffd2edd590fc1dfe35b4543194a
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-23 09:41:05 +0330

    getMessages from server was aaded as duplicate in two app and layout files.

--- app/messages/layout.js
+++ app/messages/layout.js
@@ -1,9 +1,12 @@
-export default async function MessagesLayout({ children }) {
+import { getMessages } from "@/lib/messages";
 
+export default function MessagesLayout({ children }) {
   // there is a same request in page.js file. but next just sends one of them and uses the response in all of application environment.
-  
-  const response = await fetch("http://localhost:8080/messages");
-  const messages = await response.json();
+
+  // const response = await fetch("http://localhost:8080/messages");
+  // const messages = await response.json();
+
+  const messages = getMessages();
   const totalMessages = messages.length;
 
   return (
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,20 +1,21 @@
 import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
 
 export default async function MessagesPage() {
   // there is a same request in layout.js file. but next just sends one of them and uses the response in all of application environment.
 
-  const response = await fetch("http://localhost:8080/messages", {
-    // cache: "force-cache" or "no-store"
-    // ------------------------------------------
-    // next: {
-    //   revalidate: truty | number => if number: Set the cache lifetime of a resource (in seconds).
-    // tags: [array of strings] => revalidate a specific request with specific tags instead of revalidatePath.
-    // }
-  });
-
+  //const response = await fetch("http://localhost:8080/messages", {
   // new options for fetch in next js: cach, next.
+  // cache: "force-cache" or "no-store"
+  // ------------------------------------------
+  // next: {
+  //   revalidate: truty | number => if number: Set the cache lifetime of a resource (in seconds).
+  // tags: [array of strings] => revalidate a specific request with specific tags instead of revalidatePath.
+  // }
+  //});
+  // const messages = await response.json();
 
-  const messages = await response.json();
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
